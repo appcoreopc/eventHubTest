9e6a2df34a4643959be6c0ca50ade0b82fcbbf23
jeremy woo <kepungmoin@hotmail.com>
2019-05-14 19:53:11 +1200

    init test repo

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.EventHubs;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace appDev
+{
+    class Program
+    {
+
+        private static EventHubClient eventHubClient;
+        private const string EventHubConnectionString = "Endpoint=sb://apptestevthub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Sz2HKOIWf85t62/34O/laxt3V2oUiP2HcLn2tcLIb58=";
+        private const string EventHubName = "apptestevthub";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var eventConnectionBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
+            {
+                EntityPath = EventHubName
+            };
+        
+            eventHubClient = new EventHubClient.CreateFromConnectionString(EventHubConnectionString);
+
+            await SendMessage(100);
+
+        }
+
+        private async Task SendMessage(int count) { 
+
+           var message = "sending message content "  + DateTime.Now.ToShortDateString();
+           await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+        }
+    }
+}
